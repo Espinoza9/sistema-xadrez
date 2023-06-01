fb12c261b7b48870c1a28ab1a28adf6d13ab0231
Gabriela Espinoza de Souza <gabrielasouza@dgtbr.com.br>
2023-06-01 14:53:23 -0400

    implementaçao tabuleiro e peças

---
+++ Program.java
@@ -0,0 +1,9 @@
+
+import boardgame.Tabuleiro;
+
+public class Program {
+    public static void main(String[] args) {
+    Tabuleiro tabuleiro = new Tabuleiro(8,8);
+   
+    }
+}
---
+++ boardgame/Peca.java
@@ -0,0 +1,25 @@
+package boardgame;
+
+public class Peca {
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+
+    public Peca(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+        posicao = null;
+    }
+
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Posicao posicao) {
+        this.posicao = posicao;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    
+}
